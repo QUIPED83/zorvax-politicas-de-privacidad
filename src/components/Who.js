3d056d82b1c2ad77af9d20f619902fffc59a1c44
QUIPED83 <angelq2390@gmail.com>
2024-01-23 13:06:10 -0600

    finished

--- src/components/Who.js
+++ src/components/Who.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Who() {
   return (
     <div className=" space-y-8">
-      <h2 className="text-4xl">¿Quienes somos?</h2>
+      <h2 className="text-4xl">¿Quiénes somos?</h2>
       <p className="text-xl text-justify indent-8">
         Soy un desarrollador freelancer interesado en resolver constantes
         problemas del dia a dia en mi vida y la de los de más.
