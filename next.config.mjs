45360e345352aecec979f967aa22f0f3563d375b
QUIPED83 <angelq2390@gmail.com>
2024-01-23 15:10:56 -0600

    mmm

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   output: "export",
   distDir: "dist",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
